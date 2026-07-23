c256fb6c381a3c8ebc0212e776965188317a4d94
rohitcoder24 <rohitbaghel4343@Gmail.com>
2026-07-23 10:53:05 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -30,12 +30,19 @@ module.exports = {
     ...commonConfig,
   },
 
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 4000,
-    ...commonConfig,
-  },
+production: {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 4000,
+  dialect: 'mysql',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      minVersion: 'TLSv1.2',
+      rejectUnauthorized: false
+    }
+  }
+},
 };
