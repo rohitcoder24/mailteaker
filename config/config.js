e5af402c2c1c1bb2d807dd3061d4465987595b74
rohitcoder24 <rohitbaghel4343@Gmail.com>
2026-07-23 12:13:26 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -49,38 +49,3 @@ module.exports = {
   },
 };
 
-module.exports = {
-  development: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || 'mail_tracking_dev',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 3306,
-    ...commonConfig,
-  },
-
-  test: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME_TEST || 'mail_tracking_test',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 3306,
-    ...commonConfig,
-  },
-
-production: {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 4000,
-  dialect: 'mysql',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      minVersion: 'TLSv1.2',
-      rejectUnauthorized: false
-    }
-  }
-},
-};
